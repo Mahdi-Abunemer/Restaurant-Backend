0fee5f9b320e4cf71c07715d10b3b1ed4c52c89f
Mahdi-Abunemer <163647199+Mahdi-Abunemer@users.noreply.github.com>
2025-09-29 22:02:57 +0700

    Created Dish , DishCart, Order and Rating entities classes and enums( Category , Status,DishSorting).

---
+++ RestaurantBackend.Domain/Entities/Dish.cs
@@ -0,0 +1,27 @@
+﻿using RestaurantBackend.Domain.enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace RestaurantBackend.Domain.Entities
+{
+    public class Dish
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string Photo { get; set; } = null!;
+        public double Price { get; set; }
+        public bool IsVegetarian { get; set; }
+        public float AverageRating { get; set; }
+        public Category Category { get; set; }
+        public DateTime CreateDateTime { get; set; }
+        public DateTime ModifyDateTime { get; set; }
+        public DateTime? DeleteDate { get; set; }
+        public virtual ICollection<DishCart> DishCarts { get; set; } = new List<DishCart>();
+        public virtual ICollection<Rating> Ratings { get; set; } = new List<Rating>();
+    }
+}
---
+++ RestaurantBackend.Domain/enums/DishSorting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantBackend.Domain.enums
+{
+    public enum DishSorting
+    {
+        PriceAsc,
+        PriceDesc,
+        NameAsc,
+        NameDesc,
+        RatingAsc,
+        RatingDesc
+    }
+}

20bd1ca0306ec89b566c0d716ce8ab4b27130e58
Mahdi-Abunemer <163647199+Mahdi-Abunemer@users.noreply.github.com>
2025-09-30 20:36:50 +0700

    Created ApplicationUser , ApplicationRole classes and Gender enum to be used in ApplicationUser also install Identity packge.

---
+++ RestaurantBackend.Infrastructure/Identity/ApplicationRole.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantBackend.Infrastructure.Identity
+{
+    public class ApplicationRole : IdentityRole<Guid>
+    {
+        public DateTime CreateDateTime { get; set; }
+        public DateTime ModifyDateTime { get; set; }
+        public DateTime? DeleteDateTime { get; set; }
+    }
+}
